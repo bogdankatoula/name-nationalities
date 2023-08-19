5a3531b616000a279562cc969f58e7fa32a452a7
Bogdan Katoula <bogdankatoula@hotmail.com>
2023-08-19 16:48:56 +0200

    repeat

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const h2$$ = document.createElement("h2");
 document.body.insertBefore(h2$$, input$$);
 h2$$.textContent = "Which country is your name from?";
 
+const resultContainer = document.createElement("div");
+document.body.appendChild(resultContainer);
+
 const handleClick = event => {
     event.preventDefault();
+    while (resultContainer.firstChild) {
+        resultContainer.removeChild(resultContainer.firstChild);
+    }
     const inputValue = input$$.value.trim();
     const baseUrl = `https://api.nationalize.io?name=${inputValue}`;
     fetch(baseUrl)
@@ -15,10 +21,10 @@ const handleClick = event => {
         console.log(myJson);
         if(myJson.count !==0){
         const countriesAndChances = myJson.country;
-            for (countryAndChance of countriesAndChances){
+            for (const countryAndChance of countriesAndChances){
                 console.log(countryAndChance)
                 const p$$ = document.createElement("p");
-                document.body.appendChild(p$$);
+                resultContainer.appendChild(p$$);
                 const chance = countryAndChance.probability;
                 const country = countryAndChance.country_id;
                 p$$.textContent = `The name ${inputValue} has ${chance} chances to come from ${country}`;
@@ -26,7 +32,7 @@ const handleClick = event => {
         }
         else{
             const p$$ = document.createElement("p");
-            document.body.appendChild(p$$);
+            resultContainer.appendChild(p$$);
             p$$.textContent = `Unfortunately, the name ${inputValue} doesn't exist in our database`; 
         }
     })
